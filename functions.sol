3b42cae3455757080476399927f321fc320fb26a
Bhard27 <pranjalb2709@gmail.com>
2021-02-20 03:42:24 +0530

    Adde functions solidity script

---
+++ functions.sol
@@ -0,0 +1,14 @@
+pragma solifity ^0.7.4;
+
+contract MyContract {
+  uint value;
+
+  function getValue() external view returns(uint) {
+        return value;
+
+  }
+
+  function setValue(uint _value) external {
+        value = _value;
+  }
+}
